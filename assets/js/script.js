c7a9c18143c2e71f29bca48dcd3a09a8cc3d95c7
Marc Negron <negronmarc@gmail.com>
2021-11-18 22:26:43 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,95 +26,22 @@ schedulerColor();
 
 // Local storage
 
-var textBlk1 = document.querySelector("#textblock1");
-var textBlk2 = document.querySelector("#textblock2");
-var textBlk3 = document.querySelector("#textblock3");
-var textBlk4 = document.querySelector("#textblock4");
-var textBlk5 = document.querySelector("#textblock5");
-var textBlk6 = document.querySelector("#textblock6");
-var textBlk7 = document.querySelector("#textblock7");
-var textBlk8 = document.querySelector("#textblock8");
-var textBlk9 = document.querySelector("#textblock9");
+$(".saveBtn").on("click", clickBtn);
 
-function textSave1 () {
-  textBlk1.addEventListener('input', function (event) {
-
-  localStorage.setItem("text", event.target.value);
-  });
-
-  textBlk1.value = localStorage.getItem("text");
+function clickBtn () {
+  var btn = $(this);
+  var blockEl = btn.parent();
+  var inputEl = blockEl.find("textarea");
+  var inputValue = inputEl[0].value;
+  var hour = blockEl.attr("id");
+  localStorage.setItem(hour, inputValue);
 };
 
-function textSave2 () {
-  textBlk2.addEventListener('input', function (event) {
+for (var hour = 9; hour < 18;  hour++) {
+  var blockEl = $("#hour-" + hour);
+  var inputEl = blockEl.find("textarea");
+  inputEl[0].value = localStorage.getItem("hour-" + hour);
+}
 
-  localStorage.setItem("info", event.target.value);
-  });
-
-  textBlk2.value = localStorage.getItem("info");
-};
-
-function textSave3 () {
-  textBlk3.addEventListener('input', function (event) {
-
-  localStorage.setItem("arg", event.target.value);
-  });
-
-  textBlk3.value = localStorage.getItem("arg");
-};
-
-function textSave4 () {
-  textBlk4.addEventListener('input', function (event) {
-
-  localStorage.setItem("content", event.target.value);
-  });
-
-  textBlk4.value = localStorage.getItem("content");
-};
-
-function textSave5 () {
-  textBlk5.addEventListener('input', function (event) {
-
-  localStorage.setItem("script", event.target.value);
-  });
-
-  textBlk5.value = localStorage.getItem("script");
-};
-
-function textSave6 () {
-  textBlk6.addEventListener('input', function (event) {
-
-  localStorage.setItem("body", event.target.value);
-  });
-
-  textBlk6.value = localStorage.getItem("body");
-};
-
-function textSave7 () {
-  textBlk7.addEventListener('input', function (event) {
-
-  localStorage.setItem("line", event.target.value);
-  });
-
-  textBlk7.value = localStorage.getItem("line");
-};
-
-function textSave8 () {
-  textBlk8.addEventListener('input', function (event) {
-
-  localStorage.setItem("doc", event.target.value);
-  });
-
-  textBlk8.value = localStorage.getItem("doc");
-};
-
-function textSave9 () {
-  textBlk9.addEventListener('input', function (event) {
-
-  localStorage.setItem("main", event.target.value);
-  });
-
-  textBlk9.value = localStorage.getItem("main");
-};
 
 
